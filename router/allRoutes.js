e7f0329410fb5464f85db6051e97ae3726263266
MateuszPlebanek <matprocom@gmail.com>
2024-09-18 22:36:48 +0200

    Ajout système de routage

---
+++ router/allRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Acceuil", "/pages/home.html"),];
+
+//Le titre s'affiche comme ce-ci : Route.titre - websitename
+
+export const websiteName = "Quai Antique";
