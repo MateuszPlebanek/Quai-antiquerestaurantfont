53675c6f671d2498174f2f7d7c80dad4306ab06a
MateuszPlebanek <matprocom@gmail.com>
2024-10-02 23:15:51 +0200

    galerie image

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -2,7 +2,10 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Acceuil", "/pages/home.html"),];
+    new Route("/", "Acceuil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+
+];
 
 //Le titre s'affiche comme ce-ci : Route.titre - websitename
 
