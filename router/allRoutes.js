3ab63e29e0c81512f9c7e42cdbf4ec5074730521
MateuszPlebanek <matprocom@gmail.com>
2024-11-07 23:59:15 +0100

    gestion de l'affichage des reservations

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -4,8 +4,13 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Acceuil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
-
+    new Route("/signin","Connexion","/pages/auth/signin.html"),
+    new Route("/signup","Inscription","/pages/auth/signup.html"),
+    new Route("/account","Mon compte","/pages/auth/account.html"),
+    new Route("/editPassword", "changement de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "vos réservations", "/pages/reservations/allResa.html"),
 ];
+
 
 //Le titre s'affiche comme ce-ci : Route.titre - websitename
 
